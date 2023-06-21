bc0c3053107c74865999a9eeacf6e48d721684d7
Sandun Induranga <sandutharu40@gmail.com>
2023-06-21 21:51:23 +0530

    Added Add New Customer Button

--- components/CustomersContainer.jsx
+++ components/CustomersContainer.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FiEdit2 } from "react-icons/fi";
 import { MdPlaylistRemove } from "react-icons/md";
 
 const CustomersContainer = () => {
+  const [modelOpen, setModelOpen] = useState(false);
+
   return (
     <div className="p-10 bg-gray-100 min-h-screen outline-none border-none">
-      <input
-        type="search"
-        className="lg:w-1/3 p-2 rounded-lg mb-2"
-        placeholder="Search"
-      />
+      <section className="flex sm:justify-between items-center sm:flex-row flex-col gap-4 mb-4">
+        <input
+          type="search"
+          className="lg:w-1/3 p-2 rounded-lg mb-2"
+          placeholder="Search"
+        />
+        <button className="bg-purple-100 px-4 py-2 text-purple-600 rounded-lg hover:bg-purple-600 hover:text-white">
+          Add New Customer
+        </button>
+      </section>
       <ul>
         <li className="grid grid-cols-5 justify-center text-center bg-purple-600 py-2 rounded-lg text-white mb-2">
           <h1>Customer ID</h1>
