0221a0201de4332ef49d873ab24eb9e41d5c4e03
Sandun Induranga <sandutharu40@gmail.com>
2023-06-21 13:18:03 +0530

    SlideBar Updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import SlideBar from "@/components/SlideBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="min-h-screen">
+      <SlideBar />
+    </main>
   );
 }
--- components/SlideBar.jsx
+++ components/SlideBar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SlideBar = () => {
-  return <div>SlideBar</div>;
+  return (
+    <section className="w-20 h-screen bg-white border-r border-gray-200 flex justify-center">
+      SlideBar
+    </section>
+  );
 };
 
 export default SlideBar;
