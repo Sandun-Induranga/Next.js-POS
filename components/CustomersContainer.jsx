1ef26a96c34aa4a715894b665959f581aa55a1fc
Sandun Induranga <sandutharu40@gmail.com>
2023-06-21 20:19:35 +0530

    Customer Table Header Created

--- components/CustomersContainer.jsx
+++ components/CustomersContainer.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const CustomersContainer = () => {
   return (
-    <div>
+    <div className="p-10">
       <ul>
-        <li>Customer 1</li>
+        <li className="grid grid-cols-4 justify-center text-center">
+          <h1>Customer ID</h1>
+          <h1>Customer ID</h1>
+          <h1>Customer ID</h1>
+          <h1>Customer ID</h1>
+        </li>
       </ul>
     </div>
   );
