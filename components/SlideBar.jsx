78253ec3225edc1d0766fc75902a53bbd93b35b5
Sandun Induranga <sandutharu40@gmail.com>
2023-06-21 13:45:36 +0530

    Added Liks on SlideBar

--- components/SlideBar.jsx
+++ components/SlideBar.jsx
@@ -1,9 +1,39 @@
+import Link from "next/link";
 import React from "react";
+import { RxDashboard } from "react-icons/rx";
+import { FiSlack } from "react-icons/fi";
+import { BiUser } from "react-icons/bi";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { BsCart2 } from "react-icons/bs";
 
 const SlideBar = () => {
   return (
-    <section className="w-20 h-screen bg-white border-r border-gray-200 flex justify-center">
-      SlideBar
+    <section className="w-20 h-screen bg-white border-r border-gray-200 flex items-center flex-col gap-6">
+      <FiSlack size={40} />
+      <Link href="/" className="p-2 bg-purple-600 rounded-lg text-white">
+        <RxDashboard size={22} />
+      </Link>
+
+      <Link
+        href="/customers"
+        className="p-2 bg-gray-100 rounded-lg text-gray-600 hover:bg-gray-200"
+      >
+        <BiUser size={22} />
+      </Link>
+
+      <Link
+        href="/customers"
+        className="p-2 bg-gray-100 rounded-lg text-gray-600 hover:bg-gray-200"
+      >
+        <HiOutlineShoppingBag size={22} />
+      </Link>
+
+      <Link
+        href="/customers"
+        className="p-2 bg-gray-100 rounded-lg text-gray-600 hover:bg-gray-200"
+      >
+        <BsCart2 size={22} />
+      </Link>
     </section>
   );
 };
