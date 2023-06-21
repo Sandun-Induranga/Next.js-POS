c16b921356327efcdafeebc94c11469bc8385d46
Sandun Induranga <sandutharu40@gmail.com>
2023-06-21 14:14:34 +0530

    Update Card Component

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BiUser } from "react-icons/bi";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="flex justify-between items-center lg:w-1/3 md:w-1/2 w-full border rounded px-6 py-2">
+      <div>
+        <h1 className="text-2xl font-semibold text-gray-700">Customers</h1>
+        <h4 className="text-xl text-gray-500">188</h4>
+      </div>
+      <div className="p-6 bg-purple-100 rounded-lg text-purple-600 hover:bg-gray-200">
+        <BiUser size={40} />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- components/DashboardContainer.jsx
+++ components/DashboardContainer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Card from "./Card";
 
 const DashboardContainer = () => {
-  return <div className="md:p-10 p-2">DashboardContainer</div>;
+  return (
+    <div className="md:p-10 p-2">
+      <Card />
+    </div>
+  );
 };
 
 export default DashboardContainer;
