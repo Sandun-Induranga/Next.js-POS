25c25c6df7751b39b0b95ccf888dd5ae023dfed3
Sandun Induranga <sandutharu40@gmail.com>
2023-06-21 23:03:48 +0530

    Item Table Updated

--- components/ItemsContainer.jsx
+++ components/ItemsContainer.jsx
@@ -38,42 +38,52 @@ const ItemsContainer = () => {
           className="bg-purple-100 px-4 py-2 text-purple-600 rounded-lg hover:bg-purple-600 hover:text-white"
           onClick={handleOpen}
         >
-          Add New Customer
+          Add New Item
         </button>
       </section>
       <ul>
         <li className="grid grid-cols-5 justify-center text-center bg-purple-600 py-2 rounded-lg text-white mb-2">
-          <h1>Customer ID</h1>
-          <h1>Name</h1>
-          <h1>Address</h1>
-          <h1>Phone Number</h1>
+          <h1>Item Code</h1>
+          <h1>Description</h1>
+          <h1>Qty On Hand</h1>
+          <h1>Unit Price</h1>
           <h1>Options</h1>
         </li>
         <li className="grid grid-cols-5 justify-center text-center bg-white py-2 rounded-lg text-gray-600 mt-1">
-          <h1>C00-001</h1>
-          <h1>Dasun</h1>
-          <h1>Galle</h1>
-          <h1>0762334674</h1>
+          <h1>ITM-001</h1>
+          <h1>Biscuit</h1>
+          <h1>12</h1>
+          <h1>120.00</h1>
           <div className="flex justify-center gap-4">
             <FiEdit2 size={18} className="text-green-400" />
             <MdPlaylistRemove size={18} className="text-red-400" />
           </div>
         </li>
         <li className="grid grid-cols-5 justify-center text-center bg-white py-2 rounded-lg text-gray-600 mt-1">
-          <h1>C00-001</h1>
-          <h1>Dasun</h1>
-          <h1>Galle</h1>
-          <h1>0762334674</h1>
+          <h1>ITM-001</h1>
+          <h1>Biscuit</h1>
+          <h1>12</h1>
+          <h1>120.00</h1>
           <div className="flex justify-center gap-4">
             <FiEdit2 size={18} className="text-green-400" />
             <MdPlaylistRemove size={18} className="text-red-400" />
           </div>
         </li>
         <li className="grid grid-cols-5 justify-center text-center bg-white py-2 rounded-lg text-gray-600 mt-1">
-          <h1>C00-001</h1>
-          <h1>Dasun</h1>
-          <h1>Galle</h1>
-          <h1>0762334674</h1>
+          <h1>ITM-001</h1>
+          <h1>Biscuit</h1>
+          <h1>12</h1>
+          <h1>120.00</h1>
+          <div className="flex justify-center gap-4">
+            <FiEdit2 size={18} className="text-green-400" />
+            <MdPlaylistRemove size={18} className="text-red-400" />
+          </div>
+        </li>
+        <li className="grid grid-cols-5 justify-center text-center bg-white py-2 rounded-lg text-gray-600 mt-1">
+          <h1>ITM-001</h1>
+          <h1>Biscuit</h1>
+          <h1>12</h1>
+          <h1>120.00</h1>
           <div className="flex justify-center gap-4">
             <FiEdit2 size={18} className="text-green-400" />
             <MdPlaylistRemove size={18} className="text-red-400" />
