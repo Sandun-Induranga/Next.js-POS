1b666d4e45b94d0831810e4de587a930b2739887
Sandun Induranga <sandutharu40@gmail.com>
2023-06-22 12:12:17 +0530

    Customer Model Upadted

--- backend/src/models/Customer.js
+++ backend/src/models/Customer.js
@@ -1,33 +1,19 @@
-export default class customerRepository {
-  constructor({ db }) {
-    this.db = db;
-  }
-
-  async create(customer) {
-    const createdCustomer = await this.db.Customer.create(customer);
-    return createdCustomer;
-  }
-
-  async findAll() {
-    const customers = await this.db.Customer.findAll();
-    return customers;
-  }
-
-  async findOne(id) {
-    const customer = await this.db.Customer.findOne({ where: { id } });
-    return customer;
-  }
-
-  async update(id, customer) {
-    const updatedCustomer = await this.db.Customer.update(customer, {
-      where: { id },
-      returning: true,
-    });
-    return updatedCustomer;
-  }
-
-  async delete(id) {
-    const deletedCustomer = await this.db.Customer.destroy({ where: { id } });
-    return deletedCustomer;
-  }
-}
+import { Schema, model } from "mongoose";
+const CustomerSchema = new Schema({
+  id: {
+    type: String,
+    reruired: true,
+  },
+  name: {
+    type: String,
+    reruired: true,
+  },
+  address: {
+    type: String,
+    reruired: true,
+  },
+  salary: {
+    type: Number,
+    reruired: true,
+  },
+});
